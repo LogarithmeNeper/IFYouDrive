34dab9ca5947a4ffd2f827080ca165a525a5dd36
Pierre Sibut-Bourde <53576188+LogarithmeNeper@users.noreply.github.com>
2021-04-14 19:27:07 +0200

    added radio group layout

---
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/MainActivity.kt
@@ -0,0 +1,11 @@
+package insa.lyon.h4224.ifyoudrive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
