006b31300d75cb9c0cafada6d4d6e3e7be4c1c9b
Pierre Sibut-Bourde <53576188+LogarithmeNeper@users.noreply.github.com>
2021-04-17 11:47:17 +0200

    lines not too long

--- app/src/main/java/insa/lyon/h4224/ifyoudrive/MainActivity.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/MainActivity.kt
@@ -11,37 +11,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mSexGroup: RadioGroup = findViewById(R.id.sex_group);
-        val mWeight: EditText = findViewById(R.id.weight);
-        val mBeers: EditText = findViewById(R.id.beers);
-        val mWine: EditText = findViewById(R.id.wine);
-        val mSpirits: EditText = findViewById(R.id.spirits);
-        val mDrugsGroup: RadioGroup = findViewById(R.id.drugs_group);
-        val mSubmitButton: Button = findViewById(R.id.submit_button);
+        val mSexGroup: RadioGroup = findViewById(R.id.sex_group)
+        val mWeight: EditText = findViewById(R.id.weight)
+        val mBeers: EditText = findViewById(R.id.beers)
+        val mWine: EditText = findViewById(R.id.wine)
+        val mSpirits: EditText = findViewById(R.id.spirits)
+        val mDrugsGroup: RadioGroup = findViewById(R.id.drugs_group)
+        val mSubmitButton: Button = findViewById(R.id.submit_button)
 
         mSubmitButton.setOnClickListener {
             try {
-                val sexId: Int = mSexGroup.checkedRadioButtonId;
-                var coefDiffusion: Double = 0.0;
+                val sexId: Int = mSexGroup.checkedRadioButtonId
+                var coefDiffusion: Double = 0.0
 
                 val checkedButton: RadioButton = findViewById(sexId)
                 if ("${checkedButton.text}" == "Un homme") coefDiffusion = 0.7 else coefDiffusion =
-                    0.6;
+                    0.6
 
-                val weight: Int = mWeight.text.toString().toInt();
-                val nbBeers: Int = mBeers.text.toString().toInt();
-                val nbWine: Int = mWine.text.toString().toInt();
-                val nbSpirits: Int = mSpirits.text.toString().toInt();
+                val weight: Int = mWeight.text.toString().toInt()
+                val nbBeers: Int = mBeers.text.toString().toInt()
+                val nbWine: Int = mWine.text.toString().toInt()
+                val nbSpirits: Int = mSpirits.text.toString().toInt()
 
                 val totalVolumeAlcool =
-                    nbBeers * 250 * 0.07 + nbWine * 100 * 0.12 + nbSpirits * 30 * 0.4;
-                val estimation = (totalVolumeAlcool * 0.8) / (coefDiffusion * weight);
+                    nbBeers * 250 * 0.07 + nbWine * 100 * 0.12 + nbSpirits * 30 * 0.4
+                val estimation = (totalVolumeAlcool * 0.8) / (coefDiffusion * weight)
 
-                Toast.makeText(this@MainActivity, "Estimation : $estimation", Toast.LENGTH_SHORT)
-                    .show();
+                Toast.makeText(this@MainActivity, "Estimation : $estimation",
+                    Toast.LENGTH_SHORT).show()
             }
             catch(e: Exception) {
-                Toast.makeText(this@MainActivity, "Merci de remplir tous les champs !", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this@MainActivity, "Merci de remplir tous les champs !",
+                    Toast.LENGTH_SHORT).show()
             }
         }
     }
