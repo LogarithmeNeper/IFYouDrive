7c70a44c44838198bf195127af1107f7c133535c
logarithmeneper <pierre.sibut.bourde@gmail.com>
2021-04-18 15:42:20 +0200

    added threshold for alcohol, diff answers for risk reduction

--- app/src/main/java/insa/lyon/h4224/ifyoudrive/EvaluateRisks.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/EvaluateRisks.kt
@@ -2,8 +2,11 @@ package insa.lyon.h4224.ifyoudrive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import org.w3c.dom.Text
+import kotlin.math.round
 
 class EvaluateRisks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +15,32 @@ class EvaluateRisks : AppCompatActivity() {
 
         val estimationField: TextView = findViewById(R.id.estimation_field)
         val drugsField: TextView = findViewById(R.id.drugs_field)
+        val risksField: TextView = findViewById(R.id.risks)
+        val driveButton: Button = findViewById(R.id.drive_button)
 
-        estimationField.text = intent.getStringExtra("Estimation")
-        drugsField.text = intent.getStringExtra("Drugs")
+        val estimation: Double = intent.getDoubleExtra("Estimation", 1.0)
+        val drugsEvaluation: Boolean = intent.getBooleanExtra("Drugs", true)
+        val threshold: Double = intent.getDoubleExtra("Threshold", 0.0)
+
+        estimationField.text = (round(estimation*100)/100).toString()
+        drugsField.text = drugsEvaluation.toString()
+
+        if(estimation > threshold && estimation <= 0.8) {
+            risksField.text = "Votre alcoolémie est supérieure au seuil autorisé (dans votre cas, ${threshold} g/L dans le sang). Cette contravention fait que vous risquez une amende de 135€ majorable, le retrait de 6 points sur votre permis, et une immobilisation du véhicule."
+        }
+        if(estimation > 0.8) {
+            risksField.text = "Votre alcoolémie est bien supérieur au seuil autorisé. Ce délit fait que vous risquez le retrait de votre permis, une interdiction de conduire pendant 72h, 6 points retirés sur le permis, une amende de 4500€, une peine d'emprisonnement de 2 ans maximum, une suspension voire une interdiction de permis pour une durée de 3 ans maximum.."
+        }
+        if(drugsEvaluation) {
+            risksField.text = "La conduite sous l'emprise de stupéfiants constitue un délit passible de 4500€ d'amende, d'une peine de 2 ans de prison, ainsi que d'un retrait de 6 points sur le permis de conduire automatique. Il peut aussi entraîner la suspension ou l'annulation du permis de conduire."
+        }
+
+        driveButton.isEnabled = (estimation <= threshold) && !drugsEvaluation
+        // TODO : Ajouter un bouton "+ d'infos" dans une optique de réduction des risques
+        // TODO : Faire en sorte de concaténer les deux strings plutôt que de remplacer
+
+        driveButton.setOnClickListener {
+            Toast.makeText(this@EvaluateRisks, "YEAH", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/insa/lyon/h4224/ifyoudrive/MainActivity.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val mWine: EditText = findViewById(R.id.wine)
         val mSpirits: EditText = findViewById(R.id.spirits)
         val mDrugsGroup: RadioGroup = findViewById(R.id.drugs_group)
+        val mThresholdGroup: RadioGroup = findViewById(R.id.threshold_group)
         val mEvaluateButton: Button = findViewById(R.id.evaluate_button)
         
         mEvaluateButton.setOnClickListener {
@@ -38,12 +39,19 @@ class MainActivity : AppCompatActivity() {
                 val drugsId: Int = mDrugsGroup.checkedRadioButtonId
                 val checkedDrugsButton: RadioButton = findViewById(drugsId)
 
+                val thresholdId: Int = mThresholdGroup.checkedRadioButtonId
+                val checkedThresholdButton: RadioButton = findViewById(thresholdId)
+                var threshold: Double = 0.0
+                if ("${checkedThresholdButton.text}" == "Oui") threshold = 0.2 else threshold =
+                    0.5
+
                 if(weight != 0) {
                     val estimation = (totalVolumeAlcool * 0.8) / (coefDiffusion * weight)
 
                     val intentToEvaluate = Intent(this@MainActivity, EvaluateRisks::class.java)
-                    intentToEvaluate.putExtra("Estimation", estimation.toString())
-                    intentToEvaluate.putExtra("Drugs", checkedDrugsButton.text)
+                    intentToEvaluate.putExtra("Estimation", estimation)
+                    intentToEvaluate.putExtra("Drugs", checkedDrugsButton.text=="Oui")
+                    intentToEvaluate.putExtra("Threshold", threshold)
                     startActivity(intentToEvaluate)
                 }
                 else {
