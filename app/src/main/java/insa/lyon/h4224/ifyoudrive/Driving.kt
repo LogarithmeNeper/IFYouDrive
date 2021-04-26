6ac8f2262425d1a4ba03735ac31a72b947227d88
Pierre Sibut-Bourde <53576188+LogarithmeNeper@users.noreply.github.com>
2021-04-26 09:42:40 +0200

    layout + (almost) blank activity

---
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
@@ -0,0 +1,11 @@
+package insa.lyon.h4224.ifyoudrive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Driving : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driving)
+    }
+}
