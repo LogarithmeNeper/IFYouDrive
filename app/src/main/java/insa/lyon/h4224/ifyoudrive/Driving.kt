2805bb3ea3c9ba77e7d5552db99151d538bdf13f
Pierre Sibut-Bourde <53576188+LogarithmeNeper@users.noreply.github.com>
2021-04-27 09:18:54 +0200

    better yet not working

--- app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
@@ -1,22 +1,59 @@
 package insa.lyon.h4224.ifyoudrive
 
 import android.os.Bundle
+import android.os.StrictMode
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import org.osmdroid.config.Configuration.*
 
 
 class Driving : AppCompatActivity() {
-    private val myOpenMapView: MapView? = null
-
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private lateinit var map : MapView;
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_driving)
+        super.onCreate(savedInstanceState);
+        // TODO : handle permissions
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        setContentView(R.layout.activity_main);
 
-        // TODO: as it it deprecated, find a better way to do it
-        val myOpenMapView: MapView = findViewById(R.id.mapview)
-        myOpenMapView.setBuiltInZoomControls(true)
-        myOpenMapView.setClickable(true)
-        myOpenMapView.getController().setZoom(15)
-
+        map = findViewById<MapView>(R.id.mapview)
+        map.setTileSource(TileSourceFactory.MAPNIK);
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        val permissionsToRequest = ArrayList<String>();
+        var i = 0;
+        while (i < grantResults.size) {
+            permissionsToRequest.add(permissions[i]);
+            i++;
+        }
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+
+    /*private fun requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+        if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            permissionsToRequest.add(permission);
+        }
+    }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }*/
 }
