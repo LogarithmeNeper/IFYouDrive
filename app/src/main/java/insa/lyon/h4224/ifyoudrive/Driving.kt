a13341d2454e82f3d0c18a3cf0d8a9c247393b78
Pierre Sibut-Bourde <53576188+LogarithmeNeper@users.noreply.github.com>
2021-04-27 10:37:29 +0200

    working with a generalized clear text traffic

--- app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
@@ -9,10 +9,10 @@ import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
 import android.Manifest
+import org.osmdroid.util.GeoPoint
 
 class Driving : AppCompatActivity() {
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1;
-    //private lateinit var map : MapView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         //if(hasPermissions()) {
@@ -21,6 +21,11 @@ class Driving : AppCompatActivity() {
 
             val map : MapView = findViewById(R.id.mapview)
             map.setTileSource(TileSourceFactory.MAPNIK);
+
+            val mapController = map.controller
+            mapController.setZoom(9.5)
+            val startPoint = GeoPoint(48.8583, 2.2944);
+            mapController.setCenter(startPoint);
         //}
     }
 
