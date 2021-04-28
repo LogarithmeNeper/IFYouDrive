5ced0821bb61b4c215539fe6294e87bdc81ad517
Gary F <dxsolar@yahoo.fr>
2021-04-28 10:38:04 +0200

    position affichée sur la carte + centrage sur la position au lancement

--- app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
@@ -1,32 +1,103 @@
 package insa.lyon.h4224.ifyoudrive
 
+import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.views.MapView
-import android.Manifest
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
 class Driving : AppCompatActivity() {
-    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    //private val REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+    private var firstMarker: Marker?=null
+    private var init:Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         //if(hasPermissions()) {
-            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+            Configuration.getInstance().load(
+                this, PreferenceManager.getDefaultSharedPreferences(
+                    this
+                )
+            );
             setContentView(R.layout.activity_driving);
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
             val map : MapView = findViewById(R.id.mapview)
             map.setTileSource(TileSourceFactory.MAPNIK);
+            firstMarker = Marker(map)
 
-            val mapController = map.controller
-            mapController.setZoom(9.5)
-            val startPoint = GeoPoint(48.8583, 2.2944);
-            mapController.setCenter(startPoint);
+
+        val mapController = map.controller
+            mapController.setZoom(15.0)
         //}
+        val request = LocationRequest()
+        request.interval = 10000
+        request.fastestInterval = 5000
+        request.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(request, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (ActivityCompat.checkSelfPermission(
+                        this@Driving,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this@Driving,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        latitude = if (location.latitude != null) location.latitude else latitude
+                        longitude =
+                            if (location.longitude != null) location.longitude else longitude
+                    }
+                    firstMarker!!.position = GeoPoint(latitude, longitude)
+                    firstMarker!!.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                    if (init) {
+                        mapController.setCenter(GeoPoint(latitude, longitude))
+                        map.overlays.add(firstMarker)
+                        init = false
+                    }
+                }
+            }
+        }, null)
     }
 
     private fun hasPermissions(): Boolean {
