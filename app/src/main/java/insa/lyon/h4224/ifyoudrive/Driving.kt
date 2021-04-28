650c06b6c51300bdbb901946b47875e3a58dcd24
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-04-28 08:47:01 +0200

    added a compass, a scale, and the rotation permitted

--- app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
@@ -1,28 +1,52 @@
 package insa.lyon.h4224.ifyoudrive
 
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.DisplayMetrics
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.views.MapView
-import android.Manifest
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.ScaleBarOverlay
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+
+
+
+
 
 class Driving : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-            setContentView(R.layout.activity_driving)
+        Configuration.getInstance().load(
+            this, PreferenceManager.getDefaultSharedPreferences(
+                this
+            )
+        )
+        setContentView(R.layout.activity_driving)
 
-            val map : MapView = findViewById(R.id.mapview)
-            map.setTileSource(TileSourceFactory.MAPNIK)
+        val map : MapView = findViewById(R.id.mapview)
+        map.setTileSource(TileSourceFactory.MAPNIK)
 
-            val mapController = map.controller
-            mapController.setZoom(9.5)
-            val startPoint = GeoPoint(48.8583, 2.2944)
-            mapController.setCenter(startPoint)
+        val mapController = map.controller
+        mapController.setZoom(9.5)
+        val startPoint = GeoPoint(48.8583, 2.2944)
+        mapController.setCenter(startPoint)
+
+        val compassOverlay : CompassOverlay = CompassOverlay(this, map)
+        compassOverlay.enableCompass()
+        map.overlays.add(compassOverlay)
+
+        val mRotationGestureOverlay : RotationGestureOverlay = RotationGestureOverlay(this, map)
+        mRotationGestureOverlay.setEnabled(true)
+        map.setMultiTouchControls(true)
+        map.overlays.add(mRotationGestureOverlay)
+
+        val dm : DisplayMetrics = this.resources.displayMetrics
+        val mScaleBarOverlay : ScaleBarOverlay = ScaleBarOverlay(map)
+        mScaleBarOverlay.setCentred(true)
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10)
+        map.getOverlays().add(mScaleBarOverlay)
     }
 }
