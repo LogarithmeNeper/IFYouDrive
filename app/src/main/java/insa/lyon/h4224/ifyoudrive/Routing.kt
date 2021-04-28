b0cf88f3425089b615309a7be8da97a299a8dd5a
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-04-28 11:16:43 +0200

    activity

---
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/Routing.kt
@@ -0,0 +1,11 @@
+package insa.lyon.h4224.ifyoudrive
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Routing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_routing)
+    }
+}
