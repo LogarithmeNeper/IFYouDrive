50521333c1a97871e1b2947352ffcdcbf8fcb62f
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-05-04 12:03:48 +0200

    commented everything and added a todo

--- app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
+++ app/src/main/java/insa/lyon/h4224/ifyoudrive/Driving.kt
@@ -91,12 +91,14 @@ class Driving : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // In fact, nothing working here
+        // TODO : Debug everything we do lines 97 --> 108
+        /*
         val am: AssetManager = this.assets
         val ISReader : InputStreamReader = am.open("clusterized_accidents_2017_2018_2019_lyon.csv") as InputStreamReader
         val csv = CSVReaderBuilder(ISReader).withCSVParser(CSVParserBuilder().withSeparator(';').build()).build()
 
-        // TODO : Make this thing work (even line 96 in fact)
-        /* val header = csv.readNext()
+        val header = csv.readNext()
         var line: Array<String>? = csv.readNext()
         val points: ArrayList<Pair<Double,Double>> = ArrayList()
 
@@ -104,7 +106,7 @@ class Driving : AppCompatActivity() {
             points.add(Pair(line[0].toDouble(), line[1].toDouble()))
             line=csv.readNext()
         }
-        */
+         */
 
         // Getting the shared preferences
         Configuration.getInstance().load(
